ee94044e108c32d8c81681b67b3c946ec392a142
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2026-01-13 12:01:11 +0100

    chore: update package dependencies and clean up unused files

--- sanity/schemaTypes/contactPageType.ts
+++ sanity/schemaTypes/contactPageType.ts
@@ -1,81 +1,80 @@
-import {defineArrayMember, defineField, defineType} from 'sanity'
-import {EnvelopeIcon} from '@sanity/icons'
+import { defineArrayMember, defineField, defineType } from "sanity";
+import { EnvelopeIcon } from "@sanity/icons";
 
 export const contactPageType = defineType({
-  name: 'contactPage',
-  title: 'Contact Page',
-  type: 'document',
+  name: "contactPage",
+  title: "Contact Page",
+  type: "document",
   icon: EnvelopeIcon,
-  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     defineField({
-      name: 'info_title',
-      title: 'Direct Line Title',
-      type: 'object',
+      name: "info_title",
+      title: "Direct Line Title",
+      type: "object",
       fields: [
-        defineField({name: 'de', title: 'German', type: 'string'}),
-        defineField({name: 'en', title: 'English', type: 'string'}),
-        defineField({name: 'it', title: 'Italian', type: 'string'}),
-      ]
+        defineField({ name: "de", title: "German", type: "string" }),
+        defineField({ name: "en", title: "English", type: "string" }),
+        defineField({ name: "it", title: "Italian", type: "string" }),
+      ],
     }),
     defineField({
-        name: 'address',
-        title: 'Address',
-        type: 'string',
+      name: "address",
+      title: "Address",
+      type: "string",
     }),
     defineField({
-        name: 'phone',
-        title: 'Phone Number',
-        type: 'string',
+      name: "phone",
+      title: "Phone Number",
+      type: "string",
     }),
     defineField({
-        name: 'email',
-        title: 'Email Address',
-        type: 'string',
+      name: "email",
+      title: "Email Address",
+      type: "string",
     }),
     defineField({
-      name: 'faq',
-      title: 'Frequently Asked Questions',
-      type: 'array',
+      name: "faq",
+      title: "Frequently Asked Questions",
+      type: "array",
       of: [
         defineArrayMember({
-          type: 'object',
-          name: 'faqItem',
-          title: 'FAQ Item',
+          type: "object",
+          name: "faqItem",
+          title: "FAQ Item",
           fields: [
             defineField({
-              name: 'question',
-              title: 'Question',
-              type: 'object',
+              name: "question",
+              title: "Question",
+              type: "object",
               fields: [
-                defineField({name: 'de', title: 'German', type: 'string'}),
-                defineField({name: 'en', title: 'English', type: 'string'}),
-                defineField({name: 'it', title: 'Italian', type: 'string'}),
-              ]
+                defineField({ name: "de", title: "German", type: "string" }),
+                defineField({ name: "en", title: "English", type: "string" }),
+                defineField({ name: "it", title: "Italian", type: "string" }),
+              ],
             }),
             defineField({
-              name: 'answer',
-              title: 'Answer',
-              type: 'object',
+              name: "answer",
+              title: "Answer",
+              type: "object",
               fields: [
-                defineField({name: 'de', title: 'German', type: 'text'}),
-                defineField({name: 'en', title: 'English', type: 'text'}),
-                defineField({name: 'it', title: 'Italian', type: 'text'}),
-              ]
+                defineField({ name: "de", title: "German", type: "text" }),
+                defineField({ name: "en", title: "English", type: "text" }),
+                defineField({ name: "it", title: "Italian", type: "text" }),
+              ],
             }),
-          ]
-        })
-      ]
+          ],
+        }),
+      ],
     }),
   ],
   preview: {
     select: {
-      title: 'info_title.en',
+      title: "info_title.en",
     },
-    prepare({title}) {
-        return {
-            title: title || 'Contact Page Content'
-        }
-    }
-  }
-})
+    prepare({ title }) {
+      return {
+        title: title || "Contact Page Content",
+      };
+    },
+  },
+});
